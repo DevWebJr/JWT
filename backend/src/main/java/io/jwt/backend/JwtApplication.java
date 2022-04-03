42feeb34561d49b29275052fe278a22f6098e195
Tito <50543732+DevWebJr@users.noreply.github.com>
2022-04-03 20:07:11 +0200

    add some methods on UserResource.

--- backend/src/main/java/io/jwt/backend/JwtApplication.java
+++ backend/src/main/java/io/jwt/backend/JwtApplication.java
@@ -1,7 +1,13 @@
 package io.jwt.backend;
 
+import io.jwt.backend.model.Role;
+import io.jwt.backend.model.User;
+import io.jwt.backend.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class JwtApplication {
@@ -10,4 +16,25 @@ public class JwtApplication {
 		SpringApplication.run(JwtApplication.class, args);
 	}
 
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new User(null, "Tony Stark", "Ironman", "tony", new ArrayList<>()));
+			userService.saveUser(new User(null, "Bruce Banner", "Hulk", "bruce", new ArrayList<>()));
+			userService.saveUser(new User(null, "Peter Parker", "Spiderman", "peter", new ArrayList<>()));
+			userService.saveUser(new User(null, "Steve Rogers", "Captain", "steve", new ArrayList<>()));
+
+			userService.addRoleToUser("Ironman", "ROLE_SUPER_ADMIN");
+			userService.addRoleToUser("Hulk", "ROLE_MANAGER");
+			userService.addRoleToUser("Hulk", "ROLE_USER");
+			userService.addRoleToUser("Spiderman", "ROLE_USER");
+			userService.addRoleToUser("Captain", "ROLE_ADMIN");
+		};
+	}
+
 }
+
--- backend/src/main/java/io/jwt/backend/api/UserResource.java
+++ backend/src/main/java/io/jwt/backend/api/UserResource.java
@@ -1,13 +1,15 @@
 package io.jwt.backend.api;
 
+import io.jwt.backend.model.Role;
 import io.jwt.backend.model.User;
 import io.jwt.backend.service.UserService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,4 +22,29 @@ public class UserResource {
     public ResponseEntity<List<User>>getUsers() {
         return ResponseEntity.ok().body(userService.getUsers());
     }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<User>saveUser(@RequestBody User user) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role>saveRole(@RequestBody Role role) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/role/add-to-user")
+    public ResponseEntity<?>addRoleToUser(@RequestBody RoleToUserForms form) {
+        userService.addRoleToUser(form.getUsername(), form.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+}
+
+
+@Data
+class RoleToUserForms {
+    private String username;
+    private String roleName;
 }
